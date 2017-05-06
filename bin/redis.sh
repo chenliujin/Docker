6344691d89d988313eb9479c7195209e55c93ae6
chenliujin <liujin.chen@qq.com>
2017-05-05 23:47:48 -0700

    redis

---
+++ bin/redis.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+docker run -d \
+	-v /var/lib/redis:/data \
+	-v /var/log/redis:/var/log/redis \
+	-v /etc/redis.conf:/etc/redis.conf \
+	-p 6379:6379 \
+	--name redis \
+	redis:3.2.8 \
+	redis-server /etc/redis.conf
