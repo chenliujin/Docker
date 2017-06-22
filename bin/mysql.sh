31285a901ddcaf5849abdb2db652a804e79ae41a
chenliujin <liujin.chen@qq.com>
2017-06-22 16:13:20 +0800

    mysql

--- bin/mysql.sh
+++ bin/mysql.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-docker run -d --restart=always \
+docker run \
+	-d \
+	--restart=always \
 	--name mysql \
 	-v /var/lib/mysql:/var/lib/mysql \
 	-p 3306:3306 \
-	-e MYSQL_ROOT_PASSWORD=xxxxxx \
+	-e MYSQL_ROOT_PASSWORD=chenliujin \
 	mysql:5.7.18
+
