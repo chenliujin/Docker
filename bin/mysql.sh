3d09ed50d38603c9d324ed650fa7e817839785c5
chenliujin <liujin.chen@qq.com>
2017-06-26 17:30:22 +0800

    mysql

--- bin/mysql.sh
+++ bin/mysql.sh
@@ -13,5 +13,5 @@ docker run \
 #
 # mysql > show variables like "%char%";
 
-# mysql > grant all privileges on mdc_bill.* to appuser@'%' identified by 'ab63c97e5f3e7f6';
+# mysql > grant SELECT,INSERT,UPDATE,DELETE,[CREATE,DROP] privileges on mdc_bill.* to appuser@'%' identified by 'ab63c97e5f3e7f6';
 # mysql > flush privileges;
