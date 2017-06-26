dc1a39801966995f960259d443b512e06493309f
chenliujin <liujin.chen@qq.com>
2017-06-26 16:30:03 +0800

    mysql

--- bin/mysql.sh
+++ bin/mysql.sh
@@ -4,6 +4,11 @@ docker run \
 	--restart=always \
 	--name mysql \
 	-v /data/mysql:/var/lib/mysql \
+	-v /etc/mysql/mysql.conf.d:/etc/mysql/mysql.conf.d \
 	-p 3306:3306 \
 	-e MYSQL_ROOT_PASSWORD=chenliujin \
 	mysql:5.7.18
+
+
+#
+# mysql > show variables like "%char%";
