309ed4ff70e25b3eb765dcb064a425bc4dc21d2e
chenliujin <liujin.chen@qq.com>
2017-07-06 15:24:47 +0800

    mosquitto

--- run/mosquitto.sh
+++ run/mosquitto.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+docker rm -f mosquitto
 docker run \
 	-d \
 	--name mosquitto \
 	--restart=always \
 	-v /data/mosquitto:/var/lib/mosquitto \
-	#-v /etc/mosquitto/mosquitto.conf:/etc/mosquitto/mosquitto.conf \
+	-v /etc/mosquitto/mosquitto.conf:/etc/mosquitto/mosquitto.conf \
 	-p 3000:3000 \
 	-p 1883:1883 \
+	-p 9001:9001 \
 	registry.cn-hangzhou.aliyuncs.com/chuangjike/mosquitto
