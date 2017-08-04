90c469fe7a327feb45835260794d3291afd0623c
chenliujin <liujin.chen@qq.com>
2017-08-04 11:37:40 +0800

    install.sh

--- sh/install.sh
+++ sh/install.sh
@@ -2,8 +2,10 @@
 
 yum install -y docker
 
+# 阿里云镜像加速
+echo '{"live-restore": false, "registry-mirrors":["https://2if4o8ar.mirror.aliyuncs.com"]}' > /etc/docker/daemon.json
+
 systemctl enable  docker
 systemctl restart docker
 
-# 阿里云镜像加速
-echo '{"live-restore": false, "registry-mirrors":["https://2if4o8ar.mirror.aliyuncs.com"]}' > /etc/docker/daemon.json
+
