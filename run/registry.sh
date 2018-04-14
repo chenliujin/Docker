b93bf7687826822e3ab177be1a90fdecb5cd8fc3
chenliujin <liujin.chen@qq.com>
2018-04-14 17:47:56 +0800

    registry.sh

--- run/registry.sh
+++ run/registry.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 docker rm -f registry
-docker run \
-        --name=registry \
-	-d \
-	--restart=always \
-	-p 5000:5000 \
-        -v /data/registry:/var/lib/registry \
-        -v /data/docker/auth:/auth \
-        -e "REGISTRY_AUTH=htpasswd" \
-        -e "REGISTRY_AUTH_HTPASSWD_REALM=Registry Realm" \
-        -e REGISTRY_AUTH_HTPASSWD_PATH=/auth/htpasswd \
-        -v /data/docker/certs:/certs \
-        -e REGISTRY_HTTP_TLS_CERTIFICATE=/certs/registry.crt \
-        -e REGISTRY_HTTP_TLS_KEY=/certs/registry.key \
-        registry:2.6.2
 
+docker run \
+  --name=registry \
+  -d \
+  --restart=always \
+  -p 5000:5000 \
+  -v /data/registry:/var/lib/registry \
+  -v /data/docker/auth:/auth \
+  -e "REGISTRY_AUTH=htpasswd" \
+  -e "REGISTRY_AUTH_HTPASSWD_REALM=Registry Realm" \
+  -e REGISTRY_AUTH_HTPASSWD_PATH=/auth/htpasswd \
+  -v /data/docker/certs:/certs \
+  -e REGISTRY_HTTP_TLS_CERTIFICATE=/certs/registry.crt \
+  -e REGISTRY_HTTP_TLS_KEY=/certs/registry.key \
+  registry:2.6.2
