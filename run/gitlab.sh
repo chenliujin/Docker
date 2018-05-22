a7a27ffb9bcf38e259f1f298bab4250e6b8d0b23
chenliujin <liujin.chen@qq.com>
2018-05-22 12:28:50 +0800

    gitlab.sh

--- run/gitlab.sh
+++ run/gitlab.sh
@@ -10,4 +10,4 @@ docker run \
   -e 'GITLAB_SSH_PORT=10022' \
   -v /etc/gitlab:/etc/gitlab \
   -v /var/opt/gitlab:/var/opt/gitlab \
-  docker.io/gitlab/gitlab-ce:9.5.6-ce.0
+  gitlab/gitlab-ce:gitlab/gitlab-ce
